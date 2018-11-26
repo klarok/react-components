c2903d7c215f2115d79cff0cf8c08f7e76f3e519
klarok <karaylu@gmail.com>
2018-11-26 12:04:41 -0800

    GroceryList dynamically lists GroceryListItems

--- app.js
+++ app.js
@@ -1,1 +1,23 @@
 // TODO
+
+var GroceryList = (props) => {
+	console.log(props.list);
+	return(
+		<ul>
+			{props.list.map((item) => <li>{item}</li>)}
+		</ul>
+	);
+};
+
+let Cucumbers = () => (
+	<li>Cucumbers</li>
+	);
+let Tomatoes = () => (
+	<li>Tomatoes</li>
+	);
+
+let GroceryListItem = (props) => (
+	<li>{props.item}</li>
+);
+
+ReactDOM.render(<GroceryList list={['Cucumbers', 'Tomatoes', 'Kale']}/>, document.getElementById('app'));
