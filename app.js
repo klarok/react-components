bb922168d9ba217cd94919b9822a8ab827e91eaf
klarok <karaylu@gmail.com>
2018-11-26 12:36:14 -0800

    Class component GroceryList. GroceryListItems have onClick event for toggling font weight.

--- app.js
+++ app.js
@@ -1,23 +1,47 @@
 // TODO
 
 var GroceryList = (props) => {
-	console.log(props.list);
 	return(
 		<ul>
-			{props.list.map((item) => <li>{item}</li>)}
+			{props.list.map(item => <GroceryListItem item={item} />)}
 		</ul>
 	);
 };
 
-let Cucumbers = () => (
-	<li>Cucumbers</li>
-	);
-let Tomatoes = () => (
-	<li>Tomatoes</li>
-	);
+// let Cucumbers = () => (
+// 	<li>Cucumbers</li>
+// 	);
+// let Tomatoes = () => (
+// 	<li>Tomatoes</li>
+// 	);
 
-let GroceryListItem = (props) => (
-	<li>{props.item}</li>
-);
+// let GroceryListItem = (props) => (
+// 	<li>{props.item}</li>
+// );
+
+class GroceryListItem extends React.Component {
+	constructor(props) {
+		super(props);
+
+		this.state = {
+			bought:false
+		};
+	}
+
+	onListItemHover() {
+		console.log('hi');
+		let set = !this.state.bought;
+		this.setState({bought: !this.state.bought});
+	}
+
+	render() {
+		let style = {
+			fontWeight: this.state.bought ? 'bold' : 'normal'
+		};
+		return (<li style={style} onClick={this.onListItemHover.bind(this)}>
+					{this.props.item}
+				</li>);
+	}
+}
 
 ReactDOM.render(<GroceryList list={['Cucumbers', 'Tomatoes', 'Kale']}/>, document.getElementById('app'));
